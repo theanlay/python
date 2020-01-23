8dc606f3a36e86a24beb21f3b301096612dbb161
iamtheanlay <thontheanlay@gmail.com>
2020-01-23 23:52:40 +0700

    at file number if pyhton

---
+++ python_number.py
@@ -0,0 +1,23 @@
+x=1
+y=2.8
+z=1j
+#conver form int to float:
+a=float(x)
+#conver form float to int:
+b=int(y)
+#conver form int to complex:
+c=complex(z)
+#show the vaule of a b and c
+print(a)
+print(b)
+print(c)
+# show the class of those varaible 
+print(type(a))
+print(type(b))
+print(type(c))
+
+import random
+print(random.randrange(1,10))
+
+q=int(2.0)
+w=int("3")
